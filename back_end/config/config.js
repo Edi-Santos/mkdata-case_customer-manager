010a94784d4129591905c8bff9522e9ed59b64cb
Ed Santos <edsantos2822@gmail.com>
2022-05-07 02:07:06 -0300

    build: mudando config do Sequelize na chave database para valor de variável de ambiente

--- back_end/config/config.js
+++ back_end/config/config.js
@@ -11,14 +11,14 @@ module.exports = {
   test: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: 'PSC_MKDATA_customer_manager',
+    database: process.env.MYSQL_DATABASE,
     host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
   production: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: 'PSC_MKDATA_customer_manager',
+    database: process.env.MYSQL_DATABASE,
     host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
